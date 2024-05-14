1514dc5006c31614d67062b18e85dd017d4c9c75
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-14 15:55:36 -0300

    feat(driver-application): create your entity

--- src/driver-application/entities/driver-application.entity.ts
+++ src/driver-application/entities/driver-application.entity.ts
@@ -1,1 +1,31 @@
-export class DriverApplication {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class DriverApplication {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    county: string;
+
+    @Column()
+    city: string;
+
+    @Column({ nullable: true })
+    referralCode: string;
+
+    @Column()
+    ownCar: boolean;
+
+    @Column()
+    carType: string;
+}
