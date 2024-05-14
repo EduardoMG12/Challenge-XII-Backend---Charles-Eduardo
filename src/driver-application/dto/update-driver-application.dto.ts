7893ff433ec7520b6c125bfaa597cc4ba1a3ad54
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-14 16:13:28 -0300

    refactor: remove dto unnecessary

--- src/driver-application/dto/update-driver-application.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateDriverApplicationDto } from './create-driver-application.dto';
-
-export class UpdateDriverApplicationDto extends PartialType(CreateDriverApplicationDto) {}
