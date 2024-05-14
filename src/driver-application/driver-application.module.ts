fde23e286960a78aa01cc1bbe2b6b6d3459ddf67
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-14 16:26:06 -0300

    fix: finalize implemntation driver-application

--- src/driver-application/driver-application.module.ts
+++ src/driver-application/driver-application.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DriverApplicationService } from './driver-application.service';
 import { DriverApplicationController } from './driver-application.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DriverApplication } from './entities/driver-application.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([DriverApplication])],
     controllers: [DriverApplicationController],
     providers: [DriverApplicationService],
 })
