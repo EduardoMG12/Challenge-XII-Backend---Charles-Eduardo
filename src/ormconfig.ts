cc6ed30d7656715b185d1cc12e344ca56c98a3f1
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2024-05-21 14:59:58 -0300

    fix: more adjust for building project on aws

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,14 +1,14 @@
 import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
 
 export const configAsync: TypeOrmModuleAsyncOptions = {
-  useFactory: () => ({
-    type: 'mysql',
-    host: process.env.DB_HOST,
-    port: 3306,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  }),
+    useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: false,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
 };
